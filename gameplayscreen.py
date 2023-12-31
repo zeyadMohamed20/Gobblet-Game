94005f0537836d17378b887551714dc68b610ea3
Omar Osama Abdelmonem <omar198091@beng.bu.edu.eg>
2024-01-01 01:06:34 +0200

    Add player stacks part of gameplay screen

--- gameplayscreen.py
+++ gameplayscreen.py
@@ -46,7 +46,7 @@ class BoardWindow(object):
         # Quit button
         self.quit_button = tk.Button(self.top_frame, text="Quit",font=("Supply Center", 14),bg="#997950", bd=0,activebackground="#660000", cursor="hand2", command=self.confirm_quit)
         self.quit_button.place(x=1130, y = 4)
-        
+
         # Part 2: Bottom part
         self.bottom_frame = tk.Frame(self.root, bg="#B9A180")
         self.bottom_frame.pack(fill=tk.BOTH, expand=True)
@@ -203,3 +203,91 @@ class BoardWindow(object):
                            [self.TableCell20,self.TableCell21,self.TableCell22,self.TableCell23],
                            [self.TableCell30,self.TableCell31,self.TableCell32,self.TableCell33]]
         
+        # Player 0 Gobblet Stacks area
+        self.player0_image = []
+        self.player0_image.append(ImageTk.PhotoImage(file=os.getcwd()+"\\frame0\\circle-90.png", width=4, height=4))
+        self.player0_image.append(ImageTk.PhotoImage(file=os.getcwd()+"\\frame0\\circle-70.png", width=4, height=4))
+        self.player0_image.append(ImageTk.PhotoImage(file=os.getcwd()+"\\frame0\\circle-50.png", width=4, height=4))
+        self.player0_image.append(ImageTk.PhotoImage(file=os.getcwd()+"\\frame0\\circle-30.png", width=4, height=4))
+        self.player0_stack_image = [0,0,0]
+        self.player0stack0 = tk.Button(self.bottom_frame, 
+                                               bg="#B9A180", 
+                                               activebackground="#A1662F",
+                                               disabledforeground="red",
+                                               bd=1, 
+                                               cursor="hand1", 
+                                               command=lambda: self.stack_click(Player.White, 0),
+                                               relief="sunken", width=60, height=75, image = self.player0_image[0])
+        self.player0stack0.place(x = 100, y = 450, width=100, height=100)
+
+        self.player0stack1 = tk.Button(self.bottom_frame, 
+                                               bg="#B9A180", 
+                                               activebackground="#A1662F",
+                                               disabledforeground="red",
+                                               bd=1, 
+                                               cursor="hand1", 
+                                               command=lambda: self.stack_click(Player.White, 1),
+                                               relief="sunken", width=60, height=75, image = self.player0_image[0])
+        self.player0stack1.place(x = 100, y = 330, width=100, height=100)
+
+        self.player0stack2 = tk.Button(self.bottom_frame, 
+                                               bg="#B9A180", 
+                                               activebackground="#A1662F",
+                                               disabledforeground="red",
+                                               bd=1, 
+                                               cursor="hand1",  
+                                               command=lambda: self.stack_click(Player.White, 2),
+                                               relief="sunken", width=60, height=75, image = self.player0_image[0])
+        self.player0stack2.place(x = 100, y = 210, width=100, height=100)
+
+        self.player0_stacks = [self.player0stack0,self.player0stack1,self.player0stack2]
+        self.player0_label = tk.Label(self.bottom_frame,text=f"Player 1\n{self.player1}",font=("Supply Center", 14), bg="#B9A180")
+        self.player0_label.place(x = 110, y = 130)
+
+        # Player 1 Gobblet Stacks area
+        self.player1_image = []
+        self.player1_image.append(ImageTk.PhotoImage(file=os.getcwd()+"\\frame0\\black-90.png", width=4, height=4))
+        self.player1_image.append(ImageTk.PhotoImage(file=os.getcwd()+"\\frame0\\black-70.png", width=4, height=4))
+        self.player1_image.append(ImageTk.PhotoImage(file=os.getcwd()+"\\frame0\\black-50.png", width=4, height=4))
+        self.player1_image.append(ImageTk.PhotoImage(file=os.getcwd()+"\\frame0\\black-30.png", width=4, height=4))
+        self.player1_stack_image = [0,0,0]
+        self.player1stack0 = tk.Button(self.bottom_frame, 
+                                               bg="#B9A180", 
+                                               activebackground="#A1662F",
+                                               disabledforeground="red",
+                                               bd=1, 
+                                               cursor="hand1",  
+                                               command=lambda: self.stack_click(Player.Black, 0),
+                                               relief="sunken", width=60, height=75, image = self.player1_image[0], 
+                                               state="disabled")
+        self.player1stack0.place(x = 1000, y = 50, width=100, height=100)
+
+        self.player1stack1 = tk.Button(self.bottom_frame, 
+                                               bg="#B9A180", 
+                                               activebackground="#A1662F",
+                                               disabledforeground="red",
+                                               bd=1, 
+                                               cursor="hand1",  
+                                               command=lambda: self.stack_click(Player.Black, 1),
+                                               relief="sunken", width=60, height=75, image = self.player1_image[0], 
+                                               state="disabled")
+        self.player1stack1.place(x = 1000, y = 170, width=100, height=100)
+
+        self.player1stack2 = tk.Button(self.bottom_frame, 
+                                               bg="#B9A180", 
+                                               activebackground="#A1662F",
+                                               disabledforeground="red",
+                                               bd=1, 
+                                               cursor="hand1", 
+                                               command=lambda: self.stack_click(Player.Black, 2),
+                                               relief="sunken", width=60, height=75, image = self.player1_image[0], 
+                                               state="disabled")
+        self.player1stack2.place(x = 1000, y = 290, width=100, height=100)
+        # Bind the play_click_sound function to the mouse click event for the main window
+        self.root.bind("<Button-1>", lambda event: play_click_sound())
+        self.player1_stacks = [self.player1stack0,self.player1stack1,self.player1stack2]
+        self.player1_label = tk.Label(self.bottom_frame,text=f"Player 2\n{self.player2}",font=("Supply Center", 14), bg="#B9A180")
+        self.player1_label.place(x = 1010, y = 420)
+        if self.mode == "AI VS AI":
+            for btn in self.player0_stacks:
+                btn['state'] = "disabled"
