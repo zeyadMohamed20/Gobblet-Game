4172723499438c067c00d354ae3ba7793578bf6e
NirvanaGamal <nervanagamal123@gmail.com>
2024-01-01 02:40:01 +0200

    Add Minimax Algorithm

--- Algo.py
+++ Algo.py
@@ -5,8 +5,6 @@ import random
 import time
 
 
-
-
 def generate_random_integer():
     # Use the current time as a seed
     seed_value = int(time.time())
@@ -17,6 +15,38 @@ def generate_random_integer():
 
     return random_integer
 
+def MiniMax(node, depth, maximizing_player, root_flag):
+    res_node = node
+    if depth == 0 or node.board.check_winner() == maximizing_player:
+        return node
+
+    if maximizing_player == Player.Black:
+        max_eval = float('-inf')
+        for child in node.children:
+            x_node = MiniMax(child, depth - 1, Player.White, False)
+            eval_val = x_node.Value
+            if eval_val > max_eval:
+                max_eval = eval_val
+                res_node = x_node
+                node.Value = max_eval
+        if root_flag:
+            return res_node
+        else:
+            return node
+    elif maximizing_player == Player.White:
+        min_eval = float('inf')
+        for child in node.children:
+            x_node = MiniMax(child, depth - 1, Player.Black, False)
+            eval_val = x_node.Value
+            if eval_val < min_eval:
+                min_eval = eval_val
+                res_node = x_node
+                node.Value = min_eval
+        if root_flag:
+            return res_node
+        else:
+            return node
+        
 def minimax_with_alphabeta(node, depth, maximizing_player, root_flag):
     res_node = node
     if depth == 0:
